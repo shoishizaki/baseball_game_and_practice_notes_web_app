692dbbc3fa8898870999904a304b165cf4a420c6
shoishizaki <shoishizaki1997@gmail.com>
2019-08-26 21:44:16 +0900

    Output “Hello world”

--- baseball_game_and_practice_notes_web_app/urls.py
+++ baseball_game_and_practice_notes_web_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
+    path('', include('baseball_game_and_practice_notes.urls')),
     path('admin/', admin.site.urls),
 ]
