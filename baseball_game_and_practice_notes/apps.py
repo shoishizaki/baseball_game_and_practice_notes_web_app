844912bbecef0e8890cf12f9031cf8662bc8a87b
shoishizaki <shoishizaki1997@gmail.com>
2019-08-26 21:18:15 +0900

    First commit

---
+++ baseball_game_and_practice_notes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BaseballGameAndPracticeNotesConfig(AppConfig):
+    name = 'baseball_game_and_practice_notes'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ishizakisho/baseball_game_and_practice_notes_web_app/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
