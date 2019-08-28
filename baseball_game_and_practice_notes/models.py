f07c5315f59d0fd48dcddff0585251e5b59af2f6
shoishizaki <shoishizaki1997@gmail.com>
2019-08-28 21:00:16 +0900

    Make practice note model

--- baseball_game_and_practice_notes/models.py
+++ baseball_game_and_practice_notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class PracticeNote(models.Model):
+    date = models.DateField()
+    weather = models.CharField(max_length=10)
+    practice_menu = models.TextField(max_length=1000)
+    batting = models.TextField(max_length=1000)
+    defense = models.TextField(max_length=1000)
+    running = models.TextField(max_length=1000)
+    advice = models.TextField(max_length=1000)
+    next_goal = models.TextField(max_length=1000)
