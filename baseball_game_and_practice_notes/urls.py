055595168dbfd3b88e7a868ca734fa5b330b9a89
shoishizaki <shoishizaki1997@gmail.com>
2019-08-28 22:50:39 +0900

    Make practice note html and view

--- baseball_game_and_practice_notes/urls.py
+++ baseball_game_and_practice_notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('check_practice_note/', views.check_practice_note_page, name='check_practice_note_page'),
 ]
