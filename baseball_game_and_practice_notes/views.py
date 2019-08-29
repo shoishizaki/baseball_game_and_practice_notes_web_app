0cda7fb49729726722686e0c9c44724450fc9601
shoishizaki <shoishizaki1997@gmail.com>
2019-08-30 07:31:13 +0900

    Connect practice note

--- baseball_game_and_practice_notes/views.py
+++ baseball_game_and_practice_notes/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
+from .models import PracticeNote
+
 # Create your views here.
 
 
 def index(request):
     return render(request,'baseball_game_and_practice_notes/first_page.html')
 
+
 def check_practice_note_page(request):
-    practice_note_dict = {}
-    return render(request,'baseball_game_and_practice_notes/practice_note_checking_page.html')
+    practice_note_list = PracticeNote.objects.all()
+    practice_note_dict = {'practice_note_list':practice_note_list}
+    return render(request,'baseball_game_and_practice_notes/practice_note_checking_page.html', practice_note_dict)
