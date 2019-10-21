120e8bd904e8ffdb2030f6c0ea1671a547fc20da
shoishizaki <shoishizaki1997@gmail.com>
2019-10-22 00:01:44 +0900

    Make check_practice_note_detail_page function

--- baseball_game_and_practice_notes/views.py
+++ baseball_game_and_practice_notes/views.py
@@ -14,3 +14,9 @@ def check_practice_note_page(request):
     practice_note_list = PracticeNote.objects.all()
     practice_note_dict = {'practice_note_list':practice_note_list}
     return render(request,'baseball_game_and_practice_notes/practice_note_checking_page.html', practice_note_dict)
+
+
+def check_practice_note_detail_page(request, pk):
+    practice_note_data = PracticeNote.objects.get(pk=pk)
+    practice_note_detail_dict = {'practice_note_data':practice_note_data}
+    return render(request, 'baseball_game_and_practice_notes/practice_note_detail_page.html', practice_note_detail_dict)
