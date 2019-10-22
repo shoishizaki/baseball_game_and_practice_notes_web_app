b1289fa2a003ec4c6dea020b03904d30ded7597b
shoishizaki <shoishizaki1997@gmail.com>
2019-10-23 06:38:42 +0900

    path delete function and confirm function

--- baseball_game_and_practice_notes/urls.py
+++ baseball_game_and_practice_notes/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('check_practice_note/', views.check_practice_note_page, name='check_practice_note_page'),
     path('detail_practice_note/<int:pk>/', views.check_practice_note_detail_page, name='check_practice_note_detail_page'),
     path('add_practice_note/', views.add_practice_note, name='add_practice_note_page'),
+    path('delete_practice_note/<int:pk>/', views.delete_practice_note, name='delete_function_of_practice_note'),
+    path('confirm_deletion_practice_note/<int:pk>', views.confirm_deletion_practice_note, name='confirm_deletion_practice_note_page'),
 ]
