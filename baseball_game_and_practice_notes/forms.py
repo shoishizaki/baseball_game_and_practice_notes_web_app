2257b5c044a360def70425e8977c7af77314288b
shoishizaki <shoishizaki1997@gmail.com>
2019-10-23 05:14:27 +0900

    Make practice note form

---
+++ baseball_game_and_practice_notes/forms.py
@@ -0,0 +1,67 @@
+from django import forms
+
+
+WEATHER_CHOICES = (
+        ('晴','晴'),
+        ('曇','曇'),
+        ('雨','雨'),
+        ('雪','雪'),
+)
+
+
+class PracticeNoteForm(forms.Form):
+    date = forms.DateTimeField(
+        label='date',
+        required=True,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        input_formats=['%Y-%m-%d']
+    )
+
+    weather = forms.ChoiceField(
+        label='weather',
+        widget=forms.Select,
+        choices=WEATHER_CHOICES,
+        required=True,
+    )
+
+    practice_menu = forms.CharField(
+        label='practice menu',
+        max_length=10000,
+        required=True,
+        widget=forms.TextInput()
+    )
+
+    batting = forms.CharField(
+        label='batting',
+        max_length=10000,
+        required=False,
+        widget=forms.TextInput()
+    )
+
+    defense = forms.CharField(
+        label='defense',
+        max_length=10000,
+        required=False,
+        widget=forms.TextInput()
+    )
+
+    running = forms.CharField(
+        label='running',
+        max_length=10000,
+        required=False,
+        widget=forms.TextInput()
+    )
+
+    advice = forms.CharField(
+        label='advice',
+        max_length=10000,
+        required=False,
+        widget=forms.TextInput()
+    )
+
+    next_goal = forms.CharField(
+        label='next goal',
+        max_length=10000,
+        required=True,
+        widget=forms.TextInput()
+    )
