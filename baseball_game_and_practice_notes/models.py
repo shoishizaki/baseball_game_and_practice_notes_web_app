5538acc95f67c704a4d134eeef212601b5737a0e
shoishizaki <shoishizaki1997@gmail.com>
2019-10-23 05:13:50 +0900

    Add weather choice

--- baseball_game_and_practice_notes/models.py
+++ baseball_game_and_practice_notes/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+
 class PracticeNote(models.Model):
+    WEATHER_CHOICES = (
+        ('晴','晴'),
+        ('曇','曇'),
+        ('雨','雨'),
+        ('雪','雪'),
+    )
     date = models.DateField()
-    weather = models.CharField(max_length=10)
+    weather = models.CharField(max_length=10, choices=WEATHER_CHOICES)
     practice_menu = models.TextField(max_length=1000)
     batting = models.TextField(max_length=1000, blank=True, null=True)
     defense = models.TextField(max_length=1000, blank=True, null=True)
