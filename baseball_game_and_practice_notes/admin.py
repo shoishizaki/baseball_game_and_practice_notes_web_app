28a945d003e5ef0f2220e877ed83a867b18ba150
shoishizaki <shoishizaki1997@gmail.com>
2019-10-27 16:14:06 +0900

    Make game note model

--- baseball_game_and_practice_notes/admin.py
+++ baseball_game_and_practice_notes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from baseball_game_and_practice_notes.models import PracticeNote
+from baseball_game_and_practice_notes.models import GameNote
 
 # Register your models here.
 
 admin.site.register(PracticeNote)
+admin.site.register(GameNote)
