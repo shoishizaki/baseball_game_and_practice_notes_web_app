dd20759ab18d8e31c5f1993c287b454d0ae3509c
shoishizaki <shoishizaki1997@gmail.com>
2019-11-19 07:25:02 +0900

    Change redirect

--- baseball_game_and_practice_notes/views.py
+++ baseball_game_and_practice_notes/views.py
@@ -94,7 +94,7 @@ def sign_up_func(request):
             return render(request, 'sign_up.html', {'error':'このユーザーは登録されています'})
         except:
             user = User.objects.create_user(new_username, '', new_password)
-            return render(request, 'sign_up.html')
+            return redirect('index')
     return render(request, 'sign_up.html')
 
 
